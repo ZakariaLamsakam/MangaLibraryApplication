f19c7f1e7f0f46e13a293c4fafacad4ce5457ebe
zakarialamsakam <zakaria.lamsakam@student.ehb.be>
2022-12-07 03:53:22 +0100

    Het aanmaken van de sign up pagina waar een user een account can maken.

--- src/main/java/com/example/mangalibraryapp/controller/UserController.java
+++ src/main/java/com/example/mangalibraryapp/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.mangalibraryapp.controller;
 
+import com.example.mangalibraryapp.entity.User;
 import com.example.mangalibraryapp.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -19,5 +21,11 @@ public class UserController {
     @GetMapping("")
     public String viewHomePage(){
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showSignUpForm(Model model){
+        model.addAttribute("user",new User());
+        return "signup_form";
     }
 }
