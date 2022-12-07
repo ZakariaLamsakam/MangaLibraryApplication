1450ca2cdf19d37d6676d583ab6bfb616df80a8c
zakarialamsakam <zakaria.lamsakam@student.ehb.be>
2022-12-07 01:47:38 +0100

    Het maken van MangaLibraryApp

---
+++ src/main/java/com/example/mangalibraryapp/MangaLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangalibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaLibraryAppApplication.class, args);
+    }
+
+}
